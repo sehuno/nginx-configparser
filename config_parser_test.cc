9aef2412cb79681abbd123db05ee82f7b7c360d8
sehuno <eric.sehun.oh@gmail.com>
2017-01-14 00:32:20 -0800

    added .gitignore file and unit tests for config_parser_test until first bug found

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,37 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigParserTest, ToStringTest) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("listen");
+	statement.tokens_.push_back("80");
+	EXPECT_EQ(statement.ToString(0), "listen 80;\n");
+}
+
+class NginxConfigStringTest : public ::testing::Test {
+protected:
+	bool ParseString(const std::string config_string) {
+		std::stringstream config_stream(config_string);
+		return parser.Parse(&config_stream, &out_config);
+	}
+	NginxConfigParser parser;
+	NginxConfig out_config;
+};
+
+TEST_F(NginxConfigStringTest, EmptyConfigTest) {
+	EXPECT_FALSE(ParseString(""));
+}
+
+TEST_F(NginxConfigStringTest, EmptyInsideTest) {
+	EXPECT_FALSE(ParseString("server { }"));
+}
+
+TEST_F(NginxConfigStringTest, MissingSemiColonTest) {
+	EXPECT_FALSE(ParseString("server { listen 80 }"));
+}
+
+TEST_F(NginxConfigStringTest, UnbalancedCurlyBraces) {
+	EXPECT_FALSE(ParseString("server { "));
+	EXPECT_FALSE(ParseString("server { listen 80;"));
+}
